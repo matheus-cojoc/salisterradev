6fafc7dc606aff24157e90d838c7e2ea74078ed5
Matheus Cojoc <matheus.cojoc@salisterra.ro>
2021-04-13 18:28:44 +0300

    ok

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(107, 196, 234) 0.5rem 0px 0px, rgb(107, 196, 234) -0.5rem 0px 0px',
+            backgroundColor: '#6bc4ea',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -53,11 +53,12 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(107, 196, 234) 0.5rem 0px 0px, rgb(107, 196, 234) -0.5rem 0px 0px',
+            backgroundColor: '#6bc4ea',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: 'center'
           }}
         >
           {subheading}
